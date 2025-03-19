aee7d1cd350b516405b83222dff61aa65d5da1d9
坂本大幹 <sakamototaiki@sakamototaikannoMacBook-Air.local>
2025-03-19 15:16:29 +0900

    vercel にデプロイ

--- api/index.php
+++ api/index.php
@@ -1,4 +1,6 @@
 <?php
-return function ($request, $response) {
-    $response->status(200)->send('Hello, Vercel!');
-};
+/**
+* Here is the serverless function entry
+* for deployment with Vercel.
+*/
+require __DIR__.'/../public/index.php';
